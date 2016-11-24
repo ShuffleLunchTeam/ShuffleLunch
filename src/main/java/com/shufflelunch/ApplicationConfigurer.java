7ea54b1262571fa3b102ddbfff68f05aebe5b90c
dave <sunghyun-lee@linecorp.com>
2016-11-24 14:12:08 +0900

    Delete SpringConfig use ApplicationConfigurer.

--- src/main/java/com/shufflelunch/ApplicationConfigurer.java
+++ src/main/java/com/shufflelunch/ApplicationConfigurer.java
@@ -16,15 +16,39 @@
 
 package com.shufflelunch;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.shufflelunch.config.ConfigService;
+import com.shufflelunch.config.Keys;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
+@PropertySource("classpath:application.properties")
 public class ApplicationConfigurer extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    ConfigService configService;
+
+    @Bean
+    public FirebaseAuth firebaseAuth() {
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setServiceAccount(getClass().getResourceAsStream(configService.get(Keys.FIREBASE_KEY_PATH)))
+                .setDatabaseUrl(configService.get(Keys.FIREBASE_DB_URL))
+                .build();
+        FirebaseApp.initializeApp(options);
+        return FirebaseAuth.getInstance();
+    }
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
