452c7b9e6dea5d137220e9b8585f94a496b1bf20
Schaltfehler <solako@gmx.de>
2016-11-24 18:29:03 +0900

    merged ShuffleLunchController into HelloController

--- src/main/java/com/shufflelunch/service/MessageService.java
+++ src/main/java/com/shufflelunch/service/MessageService.java
@@ -4,6 +4,8 @@ import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
@@ -12,6 +14,7 @@ import com.linecorp.bot.model.message.TextMessage;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Service
 @Slf4j
 public class MessageService {
 
