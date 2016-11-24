d802d052c615697e53c7da96c15c16cb3ab4ecc5
Schaltfehler <solako@gmx.de>
2016-11-24 18:01:02 +0900

    Added Template Message

---
+++ src/main/java/com/shufflelunch/service/MessageService.java
@@ -0,0 +1,28 @@
+package com.shufflelunch.service;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import com.linecorp.bot.model.message.TextMessage;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MessageService {
+
+    public TextMessage getWelcomeMessage(String userName) {
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Map<String, Object> params = new HashMap<>();
+        params.put("userName", userName);
+        Mustache mustache = mf.compile("message/welcomeMessage.mustache");
+        StringWriter stringWriter = new StringWriter();
+        mustache.execute(stringWriter, params);
+
+        return new TextMessage(stringWriter.toString());
+    }
+}
