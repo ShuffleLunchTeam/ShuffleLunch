9351c26da7874309d2c3e6a689a5626800f6acd4
Schaltfehler <solako@gmx.de>
2016-11-24 13:23:11 +0900

    Integrated Line Messaging SDK

--- src/main/java/com/shufflelunch/Application.java
+++ src/main/java/com/shufflelunch/Application.java
@@ -1,13 +1,24 @@
 package com.shufflelunch;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    public static Path downloadedContentDir;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    // TODO: Remove this
+    static {
+        System.setProperty("line.bot.channelSecret", "SECRET");
+        System.setProperty("line.bot.channelToken", "TOKEN");
     }
 
+    public static void main(String[] args) throws IOException {
+        downloadedContentDir = Files.createTempDirectory("line-bot");
+        SpringApplication.run(Application.class, args);
+    }
 }
