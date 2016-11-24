9412a4cb15afd1ef41d16d170fc0f169acaa639e
dave <sunghyun-lee@linecorp.com>
2016-11-24 19:08:07 +0900

    Add GroupService.

--- src/main/java/com/shufflelunch/model/User.java
+++ src/main/java/com/shufflelunch/model/User.java
@@ -2,9 +2,11 @@ package com.shufflelunch.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
     String mid;
     String name;
