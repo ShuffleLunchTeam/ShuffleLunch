9680bd1a3761c7cbc3cc1c8d33e92731fe35b362
dave <sunghyun-lee@linecorp.com>
2016-11-25 12:23:40 +0900

    Add @NoArgsConstructor to Group, Participant.

--- src/main/java/com/shufflelunch/model/Group.java
+++ src/main/java/com/shufflelunch/model/Group.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class Group {
 
     @NonNull
