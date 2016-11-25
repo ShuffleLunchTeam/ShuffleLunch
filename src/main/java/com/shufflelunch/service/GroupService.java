063a651f112b0200f98085ea93c240d9e73c48b5
dave <sunghyun-lee@linecorp.com>
2016-11-25 12:04:00 +0900

    Add group and user feature.

--- src/main/java/com/shufflelunch/service/GroupService.java
+++ src/main/java/com/shufflelunch/service/GroupService.java
@@ -1,8 +1,30 @@
 package com.shufflelunch.service;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shufflelunch.dao.FireBaseDao;
+import com.shufflelunch.model.Group;
+import com.shufflelunch.model.User;
 
 @Service
 public class GroupService {
+
+    @Autowired
+    FireBaseDao fireBaseDao;
+
+    public void addGroup(Group group) {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> groups = mapper.convertValue(group, new TypeReference<Map<String, Object>>() {});
+        fireBaseDao.update("groups/" + group.getName(), groups);
+    }
+
+    public void addUser(Group group, User user) {
+//        fireBaseDao.update("groups/" + group.getName() + "/userList", user);
+    }
 
 }
