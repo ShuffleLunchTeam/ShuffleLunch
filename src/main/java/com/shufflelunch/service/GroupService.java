b7ef951e37bcf4711d83edf7215c7bce1121494e
seisuke <seisuke@linecorp.com>
2016-11-25 14:40:25 +0900

    create shuffle algorithm

--- src/main/java/com/shufflelunch/service/GroupService.java
+++ src/main/java/com/shufflelunch/service/GroupService.java
@@ -1,5 +1,9 @@
 package com.shufflelunch.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,4 +31,60 @@ public class GroupService {
 //        fireBaseDao.update("groups/" + group.getName() + "/userList", user);
     }
 
+    public List<Integer> calcMemberNumber(int memberSize) {
+        int resultNum = memberSize;
+
+        if (resultNum <= 5) {
+            return Arrays.asList(resultNum);
+        }
+
+        List<Integer> memberNum = new ArrayList<>();
+
+        while (resultNum > 0) {
+            if (resultNum >= 4) {
+                memberNum.add(4);
+                resultNum -= 4;
+            } else {
+                memberNum.add(resultNum);
+                resultNum = 0;
+            }
+        }
+
+        while (!validGrouping(memberNum)) {
+            memberNum = rebalance(memberNum);
+        }
+
+        return memberNum;
+    }
+
+    List<Integer> rebalance(List<Integer> memberNum) {
+        int maxIndex = memberNum.indexOf(Collections.max(memberNum));
+        int minIndex = memberNum.indexOf(Collections.min(memberNum));
+        memberNum.set(maxIndex, memberNum.get(maxIndex) - 1);
+        memberNum.set(minIndex, memberNum.get(minIndex) + 1);
+        return memberNum;
+    }
+
+    boolean validGrouping(List<Integer> memberList) {
+        int max = Collections.max(memberList);
+        int min = Collections.min(memberList);
+        return max - min < 2;
+    }
+
+    public List<List<User>> grouping(List<User> userList, boolean shuffle) {
+        List<List<User>> result = new ArrayList<>();
+        List<Integer> memberNumList = calcMemberNumber(userList.size());
+        if (shuffle) {
+            Collections.shuffle(userList);
+        }
+
+        int startIndex = 0;
+        for (Integer num : memberNumList) {
+            List<User> subList = userList.subList(startIndex, startIndex + num);
+            result.add(subList);
+            startIndex += num;
+        }
+
+        return result;
+    }
 }
