424b996fd7c39c849cc29618050af6f1d55a54a1
dave <sunghyun-lee@linecorp.com>
2016-11-25 12:02:47 +0900

    Set default language of Participant.

--- src/main/java/com/shufflelunch/model/Participant.java
+++ src/main/java/com/shufflelunch/model/Participant.java
@@ -1,15 +1,15 @@
 package com.shufflelunch.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class Participant {
 
+    @NonNull
     User user;
 
-    Language language;
+    Language language = Language.JAPANESS;
 }
